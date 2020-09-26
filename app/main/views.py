8329ea9c53fd550b8006619ccf28312e020c2eaa
John Karima <karimajohn24@gmail.com>
2020-09-26 22:06:20 +0300

    add pages and respective views for updating the user profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,11 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, abort, flash
+from flask_login import login_required, current_user
 from . import main
 from ..request import get_quotes
+from ..models import User
+from .forms import UpdateProfile
+from .. import db
+
 
 @main.route('/')
 def index():
@@ -11,3 +16,32 @@ def index():
     message = 'Blog Gang Rise UP!!!'
     quotes = get_quotes()
     return render_template('index.html', message = message, title = title, quotes=quotes)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
