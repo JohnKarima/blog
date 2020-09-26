a49acb55e7be06920f01836e6538473977bc90a3
John Karima <karimajohn24@gmail.com>
2020-09-26 20:48:03 +0300

    install sqlalchemy, psycopg2

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ class Config:
     '''
     QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://qwan:123@localhost/blog'
+
 
 
 class ProdConfig(Config):
