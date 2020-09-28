ed2d887352e00454e2a61f62524720cb0d8201d9
John Karima <karimajohn24@gmail.com>
2020-09-28 12:21:05 +0300

    deployment to heroku

--- app/models.py
+++ app/models.py
@@ -22,7 +22,6 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
@@ -42,17 +41,6 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
-
-
-    def __repr__(self):
-        return f'User {self.name}'
 
 
 class Blog(db.Model):
